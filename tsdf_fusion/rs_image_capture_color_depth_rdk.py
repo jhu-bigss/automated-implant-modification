c727555823032bc944c550ded29a06ba093c3377
Joshua Liu <liushuya7@gmail.com>
2020-12-04 18:40:57 -0500

    add tsdf pkg

---
+++ tsdf_fusion/rs_image_capture_color_depth_rdk.py
@@ -0,0 +1,110 @@
+"""
+RoboDK capture images with RealSense camera based on PyQt
+"""
+
+import os, sys, shutil
+
+from PyQt5 import Qt, QtCore
+
+from robolink import *
+from robodk import *
+
+from utils.camerathread import CameraThread
+
+data_foler = 'data/'
+
+class MainWidget(Qt.QWidget):
+    rdk = Robolink()
+    image_captured = QtCore.pyqtSignal(str)
+    window_closed = QtCore.pyqtSignal()
+
+    def __init__(self, name=None, parent=None, show=True):
+        super(MainWidget, self).__init__()
+        self.setWindowTitle(name)
+
+        # create a label to display camera image
+        self.cv_label = Qt.QLabel()
+        cv_thread = CameraThread(self)
+        cv_thread.change_pixmap.connect(self.set_image)
+        cv_thread.start()
+
+        # create default save folder
+        self.save_directory = self.create_data_directory(os.path.dirname(os.path.realpath(__file__)))
+
+        self.open_dir_button = Qt.QPushButton("Save Folder")
+        self.capture_button = Qt.QPushButton("Capture")
+        self.calibrate_button = Qt.QPushButton("Run TSDF")
+
+        self.open_dir_button.clicked.connect(self.open_data_directory)
+        self.capture_button.clicked.connect(self.capture_event)
+        self.calibrate_button.clicked.connect(self.run_tsdf_event)
+
+        vlayout = Qt.QVBoxLayout()
+        vlayout.addWidget(self.cv_label)
+        hlayout_1 = Qt.QHBoxLayout()
+        hlayout_1.addWidget(self.open_dir_button)
+        hlayout_1.addWidget(self.capture_button)
+        hlayout_1.addWidget(self.calibrate_button)
+        vlayout.addLayout(hlayout_1)
+        vlayout.setContentsMargins(0,0,0,0)
+        self.setLayout(vlayout)
+
+        # RoboDK stuff
+        self.robot = self.rdk.Item('', ITEM_TYPE_ROBOT)
+        self.pose_counter = 0
+
+        if show:
+            self.show()
+
+    def closeEvent(self, event):
+        reply = Qt.QMessageBox.question(self, 'Window Close', 'Are you sure you want to close this window?',
+                Qt.QMessageBox.Yes | Qt.QMessageBox.No, Qt.QMessageBox.No)
+
+        if reply == Qt.QMessageBox.Yes:
+            # signal camera thread to stop camera
+            self.window_closed.emit()
+            event.accept()
+            print('Window closed')
+        else:
+            event.ignore()
+
+    @Qt.pyqtSlot(Qt.QImage)
+    def set_image(self, image):
+        self.cv_label.setPixmap(Qt.QPixmap.fromImage(image))
+
+    # create a directory to save captured images and robot poses
+    def create_data_directory(self, dir):
+        dir = os.path.join(dir, data_foler)
+        try:
+            if not(os.path.isdir(dir)):
+                os.makedirs(dir)
+        except OSError as e:
+            print("Can't make the directory: %s" % dir)
+            raise
+        return dir 
+    
+    # open a new directory for saving data
+    def open_data_directory(self):
+        dir_open = Qt.QFileDialog.getExistingDirectory(self, 'Select Save Folder', os.path.dirname(os.path.realpath(__file__)))
+        self.save_directory = self.create_data_directory(dir_open)
+
+    # call the opencv thread to save image to the given directory
+    def capture_event(self):
+        self.image_captured.emit(self.save_directory)
+
+        # save the robot's current pose
+        robot_pose = self.robot.Pose()
+        f_name = str(self.pose_counter) + '.txt'
+        robot_pose.SaveMat(f_name)
+        shutil.move(os.path.join(os.getcwd(), f_name), self.save_directory)
+        print('robot pose: ' + str(self.pose_counter) + '.txt')
+        self.pose_counter += 1
+
+    def run_tsdf_event(self):
+        pass
+
+if __name__ == '__main__':
+    app = Qt.QApplication(sys.argv)
+    window_title="OpenCV Hand-eye Calibration"
+    window = MainWidget(window_title)
+    sys.exit(app.exec_())
---
+++ tsdf_fusion/utils/camerathread.py
@@ -0,0 +1,97 @@
+import os
+
+from PyQt5 import Qt, QtCore
+
+import cv2
+import pyrealsense2 as rs
+
+import numpy as np
+
+DEFAULT_WIDTH = 1280
+DEFAULT_HEIGHT = 720
+DEFAULT_FPS = 30
+
+class CameraThread(QtCore.QThread):
+
+    change_pixmap = QtCore.pyqtSignal(Qt.QImage)
+
+    def __init__(self, parent=None, width=DEFAULT_WIDTH, height=DEFAULT_HEIGHT, fps=DEFAULT_FPS):
+        super().__init__()
+        
+        # Declare RealSense pipelineline 
+        self.pipeline = rs.pipeline()
+        config = rs.config()
+        config.enable_stream(rs.stream.depth, width, height, rs.format.z16, fps)
+        config.enable_stream(rs.stream.color, width, height, rs.format.bgr8, fps)
+        
+        # start streaming
+        self.pipeline.start(config)
+        self.running = True
+        print("start realsense!")
+        
+        # Create an align object
+        # rs.align allows us to perform alignment of depth frames to others frames
+        self.align = rs.align(rs.stream.color) # align to color frame
+
+        parent.image_captured.connect(self.save_image)
+        parent.window_closed.connect(self.stop)
+
+        self.image_counter = 0 # saving images
+
+    def run(self):
+        try:
+            while self.running:
+                frames = self.pipeline.wait_for_frames()
+
+                # Align the depth frame to color frame
+                aligned_frames = self.align.process(frames)
+
+                # Get aligned depth frames
+                depth_frame = aligned_frames.get_depth_frame()
+
+                # depth_frame = frames.get_depth_frame()
+                color_frame = frames.get_color_frame()
+                # depth_frame = frames.get_depth_frame()
+                if not depth_frame or not color_frame:
+                    continue
+
+                # convert image to numpy arr
+                self.depth_image = np.asanyarray(depth_frame.get_data())
+                self.color_image = np.asanyarray(color_frame.get_data())
+
+                # depth image must be converted to 8-bit per pixel first
+                depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(self.depth_image, alpha=0.03), cv2.COLORMAP_JET)
+
+                # Stack both images horizontally
+                images = np.hstack((self.color_image, depth_colormap))
+
+                # convert BGR (opencv) to RGB (QImage)
+                # ref: https://stackoverflow.com/a/55468544/6622587
+                color_image = cv2.cvtColor(images, cv2.COLOR_BGR2RGB)
+
+                h, w, ch = images.shape
+                bytesPerLine = ch * w
+                color_qimage = Qt.QImage(color_image.data, w, h, bytesPerLine, Qt.QImage.Format_RGB888)
+                color_qimage = color_qimage.scaled(640 * 2, 480, QtCore.Qt.KeepAspectRatio)
+                self.change_pixmap.emit(color_qimage)
+
+        finally:
+            # stop streaming
+            # self.pipeline.stop()
+            pass
+
+    @Qt.pyqtSlot()
+    def stop(self):
+        self.running = False
+        self.pipeline.stop()
+        print("close realsense.")
+        self.quit()
+
+    @Qt.pyqtSlot(str)
+    def save_image(self, save_dir):
+        depth_image_path = os.path.join(save_dir, 'depth-' + str(self.image_counter) + '.png')
+        color_image_path = os.path.join(save_dir, 'color-' + str(self.image_counter) + '.png')
+        cv2.imwrite(depth_image_path, self.depth_image)
+        cv2.imwrite(color_image_path, self.color_image)
+        print('image saved: ' + str(self.image_counter))
+        self.image_counter += 1
