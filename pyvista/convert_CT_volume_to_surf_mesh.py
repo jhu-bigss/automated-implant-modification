daa38fc9d3d6ec568d29903dbd08962badfbd2af
Joshua Liu <liushuya7@gmail.com>
2021-01-12 18:31:17 -0500

    add parse input mesh as a command-line argument

--- pyvista/convert_CT_volume_to_surf_mesh.py
+++ pyvista/convert_CT_volume_to_surf_mesh.py
@@ -2,17 +2,24 @@ import pyvista as pv
 import numpy as np
 
 import os
+import argparse
 
 # This file is a pre-processor to take a CT mesh model then remove the inner layer of the mesh
 # for registration from CT to 3d scan of the patien's head
+# Usage:
+#       python convert_CT_volume_to_surf_mesh.py input_mesh_file_path
 
-self_dir = os.path.dirname(os.path.realpath(__file__))
-data_dir = self_dir + "/../Data/"
-input_file = "skull_5.stl"
-output_file = "skull_5_outer_surface.stl"
+parser = argparse.ArgumentParser(description='Convert CT volume mesh to a single-layer surface mesh')
+parser.add_argument('mesh_file', metavar='INPUT_FILE', help='Input mesh file path')
+args = parser.parse_args()
+
+input_file = args.mesh_file
+data_dir = os.path.dirname(input_file)
+input_file_name = os.path.basename(input_file)
+output_file = input_file_name.split('.')[0] + "_outer_surface.stl"
 
 # load CT mesh
-mesh_ct = pv.read(data_dir + input_file)
+mesh_ct = pv.read(input_file)
 
 # construct vectors from center to each cell centers, then perform element-wise dot product
 removals = []
@@ -28,4 +35,6 @@ mesh_ct.remove_cells(removals < 0)
 mesh_ct.extract_largest(inplace=True)
 
 # output the mesh
-mesh_ct.save(data_dir + output_file, binary=True)
+mesh_ct.save(data_dir + '/' + output_file, binary=True)
+
+print("Done!")
